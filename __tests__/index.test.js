ea45733deeb5a2e27e7e41217cf94397fa890e58
Anton <web-gambit@mail.ru>
2020-05-02 19:14:21 +0500

    Add tests for the text format

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,18 +10,24 @@ beforeAll(() => {
 });
 
 test.each([
-  ['plain', '.json'],
-  ['plain', '.yml'],
-  ['plain', '.ini'],
-  ['nested', '.json'],
-  ['nested', '.yml'],
-  ['nested', '.ini'],
-])('A diff for %s %s files is correct', (kind, extension) => {
+  ['plain', '.json', 'tree'],
+  ['plain', '.yml', 'tree'],
+  ['plain', '.ini', 'tree'],
+  ['nested', '.json', 'tree'],
+  ['nested', '.yml', 'tree'],
+  ['nested', '.ini', 'tree'],
+  ['plain', '.json', 'text'],
+  ['plain', '.yml', 'text'],
+  ['plain', '.ini', 'text'],
+  ['nested', '.json', 'text'],
+  ['nested', '.yml', 'text'],
+  ['nested', '.ini', 'text'],
+])('A diff for %s %s files is correct [%s format]', (kind, extension, format) => {
   const beforeFilePath = path.join(fixturesPath, kind, `before${extension}`);
   const afterFilePath = path.join(fixturesPath, kind, `after${extension}`);
-  const diffFilePath = path.join(fixturesPath, kind, 'diff');
+  const diffFilePath = path.join(fixturesPath, kind, `diff.${format}`);
   const diffFileContents = fs.readFileSync(diffFilePath, 'utf-8').trimEnd();
 
-  expect(compareTwoFiles(beforeFilePath, afterFilePath))
+  expect(compareTwoFiles(beforeFilePath, afterFilePath, format))
     .toBe(diffFileContents);
 });
